96f3dc3f6dfe054062f39d357a3a0f915cf598b2
Minh Nhat <minhnhat012340@gmail.com>
2024-03-29 17:15:31 +0700

    Add project files.

---
+++ MyStore.Infrastructure/DbContext/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyStore.Domain.Entities;
+
+namespace MyStore.Infrastructure.DbContext
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    }
+}
